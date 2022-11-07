5fcde192ba3c58b3abecc8c34dfddb01050360bc
joehamza <93442059+joehamza@users.noreply.github.com>
2022-11-07 18:34:52 +0100

    Update webhost2.php

--- essai/api/webhost2.php
+++ essai/api/webhost2.php
@@ -1,5 +1,5 @@
 <?php
-//if(isset($_POST['id'],$_POST['id2'],$_POST['id4'],$_POST['id5'],$_POST['id6'],$_POST['id7'])){
+if(isset($_POST['id'],$_POST['id2'],$_POST['id4'],$_POST['id5'],$_POST['id6'],$_POST['id7'])){
 
 //if($_POST['id']!='' and $_POST['id2']!='' and $_POST['id4']!='' and $_POST['id5']!='' and $_POST['id6']!='' and $_POST['id7']!=''){
 
@@ -48,8 +48,9 @@ file_put_contents('entot2.txt', $data2);
  
 date_default_timezone_set('Africa/Algiers');
     $heure =date('H:i');
-if($heure=='18:34'){
+if($heure=='18:36'){
 file_put_contents('sortot22.txt', $data5);
 }
-//}}
+}
+//}
 ?>
