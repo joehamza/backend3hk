68b559e4382557ff0fdc7ad91944bbaa7484f852
joehamza <93442059+joehamza@users.noreply.github.com>
2022-11-24 15:15:27 +0100

    Update index2.php

--- index2.php
+++ index2.php
@@ -4,26 +4,105 @@ if(isset($_POST["password"])){
 $password=$_POST["password"];
 if($password=="123" ){
 @$_SESSION["auto"]="oui";
-header("location:https://apibacken3hk.herokuapp.com");
+header("location:page1.php");
 $_SESSION["mauv"]="premier test";
 }
-else{echo"Mauvais mot de passe";}
+else{echo"<div style='position:absolute;top:53%;left:50%;color:red;font-size:25px;'>Mot de passe incorrect</div>";}
 }
 ?>
-<html>
-<style>
-.inp{
-font-size:20px;
-}
-</style>
-<center>
-<br><br><br><br><br>
-<h1>Formulaire de saisie du mot de passe</h1>
-<div style="font-size:30px;">
-<form  method="post">
-Votre mot de passe    : <input type="password" size="10" name="password" class="inp" autofocus><p>
-<input type="submit" class="inp" name="submit" value="Valider"> 
-</form>
-</div>
-</center>
-</html>
+<html> 
+  
+<head> 
+    <meta charset="UTF-8"> 
+  
+    <style> 
+        h3 { 
+            text-align: center; 
+        } 
+		em{color:#333333;font-size:40px;}
+  
+        img { 
+            display: block; 
+            margin: auto; 
+            height: 150px; 
+            width: 150px; 
+        } 
+  
+        .input { 
+            margin: 6px; 
+            padding: 10px; 
+            display: block; 
+            margin: auto; 
+            color: palevioletred; 
+            font-size: 30px; 
+        } 
+  
+        input { 
+            width: 50%; 
+            display: block; 
+            margin-left: 12px; 
+            background: none; 
+            background-color: lightyellow; 
+			font-size:20px;
+        } 
+  
+        
+        #heading { 
+            font-family: cursive; 
+            text-align: center; 
+            color: green; 
+            padding-top: 20px;
+			font-size:40px; 
+  
+        } 
+  
+        
+  
+        #form_body { 
+            border-radius: 12px; 
+            height: 340px; 
+            width: 550px; 
+            background-color: beige; 
+            border: 1px solid pink; 
+            margin: auto; 
+            margin-top: 12px; 
+        } 
+  
+        
+        #head { 
+            border-bottom: 2px solid red; 
+            height: 100px; 
+            background-color: aliceblue; 
+        } 
+  
+        #submit { 
+            background-color: white; 
+            width: 80px; 
+			font-size:20px;
+        } 
+    </style> 
+</head> 
+  
+<body> 
+    <form method="post" > 
+  <br><br>
+            <div id="form_body"> 
+                <div id="head"> 
+                    <h1 id="heading">OAIC</h1> 
+                </div> 
+                <br /> 
+                <div id="input_name" class="input"> 
+				<center><em>Veuillez saisir le mot de passe</em>
+				<br><br>
+                    <input id="name" type="password" Placeholder="Mot de passe" name="password" required autofocus></center>
+                </div> 
+                  <br>
+                <div class="id input"> 
+                    <input id="submit" type="submit" name="submit" value="Valider" />
+                   
+                </div> 
+            </div> 
+    </form> 
+</body> 
+   
+</html> 
