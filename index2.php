71640567cd33d9785a88c523f31507beb45567c4
joehamza <93442059+joehamza@users.noreply.github.com>
2022-11-24 11:17:10 +0100

    Update index2.php

--- index2.php
+++ index2.php
@@ -4,7 +4,7 @@ if(isset($_POST["password"])){
 $password=$_POST["password"];
 if($password=="123" ){
 @$_SESSION["auto"]="oui";
-header("location:page1.php");
+header("location:index.php");
 $_SESSION["mauv"]="premier test";
 }
 else{echo"Mauvais mot de passe";}
